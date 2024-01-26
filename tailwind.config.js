4a3b0c303e1396cbb7ebfb3847b6517e1a96690d
Creiwry <jay.gardos@outlook.com>
2024-01-26 17:54:39 +0100

    config(frontend): set up react router for app

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.jsx',
     './app/javascript/**/*.js'
   ]
 }
